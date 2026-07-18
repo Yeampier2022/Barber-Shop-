1540765ab8daf5ac51f3870b55e2d3c83925985a
Madison Thomas <madison.thomas094@gmail.com>
2026-07-18 17:51:37 -0600

    render 7-day strip

--- src/screens/AppointmentScreen.tsx
+++ src/screens/AppointmentScreen.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+import { WeekStrip } from "../components/appointments/DateSelect/WeekStrip";
+
 // Barber Info Header
 
 // Service Selector
 
 // Date Selector
-
+const [selectedDate, setSelectedDate] = useState(new Date());
+const[weekStart, setWeekStart] = useState(new Date());
+<WeekStrip
+  selectedDate={selectedDate}
+  weekStart={weekStart}
+  onSelectDate={setSelectedDate}
+/>
 
 // Time Selector
 
