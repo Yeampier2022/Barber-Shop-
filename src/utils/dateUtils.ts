477af34257f33ac1450c47efa539cd1d044b2b0d
Madison Thomas <madison.thomas094@gmail.com>
2026-07-20 13:55:04 -0600

    utility functions for displaying month

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,11 +1,17 @@
 import {
     addDays,
+    endOfMonth,
+    endOfWeek,
     format,
+    isSameMonth as dfIsSameMonth,
     isSameDay as dfIsSameDay,
     isToday as dfIsToday,
     startOfDay,
+    startOfWeek,
+    startOfMonth,
 } from "date-fns";
 
+const BOOKING_WINDOW_DAYS = 30; // How far out can a booking be made
 
 // Returns an array of dates beginning at startDate.
 export function getNextDays(
@@ -38,4 +44,38 @@ export function formatDay(date: Date) {
 
 export function formatMonth(date: Date) {
     return format(date, "MMMM yyyy"); // July 2026
+}
+
+// Returns an array of dates that would be displayed in a standard month calendar view
+export function getMonthGrid(month: Date): Date[] {
+    const start = startOfWeek(startOfMonth(month));
+    const end = startOfWeek(endOfMonth(month));
+    const dates: Date[] = [];
+
+    let current = start;
+    while (current <= end) {
+        dates.push(current);
+        current = addDays(current, 1);
+    }
+    return dates;
+}
+
+// Determines if the month provided is the same as the month of the date (for display purposes)
+export function isSameMonth(date: Date, month: Date) {
+    return dfIsSameMonth(date, month);
+}
+
+// Returns what dates are in the booking window
+export function getBookingWindow() {
+    const today = startOfDay(new Date());
+    return {
+        minDate: today,
+        maxDate: addDays(today, BOOKING_WINDOW_DAYS),
+    }
+}
+
+// Compares date to the booking window (for display purposes)
+export function isDateInWindow(date: Date) {
+    const window = getBookingWindow();
+    return date >= window.minDate && date <= window.maxDate;
 }
