c8c7810c131d48ad1437f94ea956c3b5d7300190
Madison Thomas <madison.thomas094@gmail.com>
2026-07-23 14:17:36 -0600

    mock appointment data for testing

--- src/mocks/appointments.ts
+++ src/mocks/appointments.ts
@@ -1,19 +1,55 @@
-import { addMinutes } from "date-fns";
-import { Appointment } from "../types/schedule";
+import { set } from "date-fns";
+import type { Appointment } from "../types/schedule";
 
 export function getMockAppointments(day: Date): Appointment[] {
   return [
     {
-      barberId: "barber1",
-      clientId: "client1",
-      start: day,
-      end: addMinutes(day, 30),
+      barberId: "barber-1",
+      clientId: "client-1",
+      start: set(day, {
+        hours: 9,
+        minutes: 30,
+        seconds: 0,
+        milliseconds: 0,
+      }),
+      end: set(day, {
+        hours: 10,
+        minutes: 0,
+        seconds: 0,
+        milliseconds: 0,
+      }),
     },
     {
-      barberId: "barber2",
-      clientId: "client2",
-      start: day,
-      end: addMinutes(day, 30),
+      barberId: "barber-1",
+      clientId: "client-2",
+      start: set(day, {
+        hours: 11,
+        minutes: 0,
+        seconds: 0,
+        milliseconds: 0,
+      }),
+      end: set(day, {
+        hours: 12,
+        minutes: 0,
+        seconds: 0,
+        milliseconds: 0,
+      }),
+    },
+    {
+      barberId: "barber-1",
+      clientId: "client-3",
+      start: set(day, {
+        hours: 13,
+        minutes: 0,
+        seconds: 0,
+        milliseconds: 0,
+      }),
+      end: set(day, {
+        hours: 13,
+        minutes: 45,
+        seconds: 0,
+        milliseconds: 0,
+      }),
     },
   ];
 }
