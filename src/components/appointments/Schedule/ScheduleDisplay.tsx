b6c91abbb373fcf0b67451ceb9ff42032ed56a76
Madison Thomas <madison.thomas094@gmail.com>
2026-07-23 12:43:52 -0600

    props and function init

--- src/components/appointments/Schedule/ScheduleDisplay.tsx
+++ src/components/appointments/Schedule/ScheduleDisplay.tsx
@@ -3,3 +3,22 @@ import { View } from "react-native";
 import { getTimeSlots, getSlotState } from "../../../utils/scheduleUtils";
 import type { TimeSlot } from "../../../types/schedule";
 
+type ScheduleDisplayProps = {
+  day: Date;
+  startHour: number;
+  endHour: number;
+  slotLength: number;
+  selectedStartTime: Date | null;
+  onSlotPress: (slot: TimeSlot) => void;
+};
+
+export function ScheduleDisplay({
+  day,
+  startHour,
+  endHour,
+  slotLength,
+  selectedStartTime,
+  onSlotPress,
+}: ScheduleDisplayProps) {
+  
+}
